8c8ba6c2ee6bfffe3481aae260b500145b04b9ac
Doktor Ziel <doktor@zielony-backlog.pl>
2022-12-06 07:43:33 +0100

    But we can add force creation/deletion order by adding `@DependsOn` annotation

--- src/main/java/com/griddynamics/tech3camp/santa/claus/workshop/digression/DigressionConfiguration.java
+++ src/main/java/com/griddynamics/tech3camp/santa/claus/workshop/digression/DigressionConfiguration.java
@@ -3,12 +3,14 @@ package com.griddynamics.tech3camp.santa.claus.workshop.digression;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.DependsOn;
 
 @Configuration
 @ConditionalOnProperty(value = "digression", havingValue = "true")
 public class DigressionConfiguration {
 
     @Bean(initMethod = "init", destroyMethod = "cleanUp")
+    @DependsOn("justGlue")
     public Dictionary polishDictionary() {
         return new Dictionary("/dictionary_pl.txt");
     }
